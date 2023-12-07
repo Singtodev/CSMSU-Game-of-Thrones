296e65ced9adb143d2f9fc53d26516179e47a7e8
Unkeloc4 <138797225+Unkeloc4@users.noreply.github.com>
2023-12-07 14:50:56 +0700

    Content in page

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , HeaderComponent],
+  imports: [CommonModule, RouterOutlet , HeaderComponent , FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'code';
+
+  faHearth = faHeart;
+
 }
